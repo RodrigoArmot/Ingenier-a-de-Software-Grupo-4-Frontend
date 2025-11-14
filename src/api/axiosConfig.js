e3429b182d8ff8dbbcfb69e88d0dcaa661d2e091
CardenasAxel <cardenas.axel@pucp.edu.pe>
2025-11-14 17:39:36 -0500

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://98.92.241.39:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
